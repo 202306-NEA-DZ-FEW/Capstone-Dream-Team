b1d599f588ba160563f8a6152a615654dff59216
MerzoukFatima <merzoukfatimazahraa@gmail.com>
2023-11-01 15:09:15 +0100

    feat(blog page): add similar blogs section in [blog] page
    #12

--- src/pages/blogs/[blog].jsx
+++ src/pages/blogs/[blog].jsx
@@ -1,156 +1,138 @@
+import {
+    collection,
+    doc,
+    getDoc,
+    getDocs,
+    query,
+    where,
+} from "firebase/firestore";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import React from "react";
 
-import Layout from "@/layout/Layout";
+import BlogCardList from "@/components/Blog/BlogCard/blogCardList";
 
-export default function Blog() {
+import Layout from "@/layout/Layout";
+import { db } from "@/util/firebase";
+
+export default function Blog({ blog, similarBlogs }) {
+    const { t } = useTranslation("common");
+    const router = useRouter();
+    const paragraphs = blog.content.split("\n\n");
     return (
         <Layout>
-            <div>
-                {/* Container */}
-                <div className='container w-full md:max-w-3xl mx-auto pt-10'>
-                    <div
-                        className='w-full px-4 md:px-6 text-base text-gray-800 leading-normal'
-                        style={{ fontFamily: "Georgia, serif" }}
-                    >
-                        {/* Title */}
-                        <div className='font-sans'>
-                            <p className='text-base md:text-sm text-green-500 font-bold'>
-                                &lt;{" "}
-                                <Link
-                                    href='/blogs'
-                                    className='text-base md:text-sm text-green-500 font-bold no-underline hover:underline'
-                                >
-                                    BACK
-                                </Link>
-                            </p>
-                            <h1 className='font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl'>
-                                Welcome to Minimal Blog
-                            </h1>
-                            <p className='text-sm md:text-base font-normal text-gray-600'>
-                                Published 19 February 2019
-                            </p>
-                        </div>
-                        <div className='py-3'>
-                            <img
-                                src='/images/fatoom_in_yemen.png'
-                                alt='Image Description'
-                                className=' block'
-                            />
-                        </div>
-                        {/* Post Content */}
-                        {/* Lead Para */}
-                        <p className='py-6'>
-                            👋 Welcome fellow Tailwind CSS and minimal
-                            monochrome blog fan. This starter template provides
-                            a starting point to create your own minimal
-                            monochrome blog using Tailwind CSS and vanilla
-                            Javascript.
+            {/* Container */}
+            <div className='container dark:text-white w-full md:max-w-3xl mx-auto pt-10'>
+                <div
+                    className='w-full px-4 md:px-6 text-base text-gray-800 leading-normal'
+                    style={{ fontFamily: "Georgia, serif" }}
+                >
+                    {/* Title */}
+                    <div className='font-sans'>
+                        <p className='text-base md:text-sm text-green-500 font-bold'>
+                            &lt;{" "}
+                            <Link
+                                href='/blogs'
+                                className='text-base md:text-sm text-green-500 font-bold no-underline hover:underline'
+                            >
+                                {t("blogPage.blog.back")}
+                            </Link>
                         </p>
-                        <p className='py-6'>
-                            The basic blog page layout is available and all
-                            using the default Tailwind CSS classNamees (although
-                            there are a few hardcoded style tags). If you are
-                            going to use this in your project, you will want to
-                            convert the classNamees into components.
+                        <h1 className='dark:text-white font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl'>
+                            {blog.title}
+                        </h1>
+                        <p className='dark:text-white text-sm md:text-base font-normal text-gray-600'>
+                            {t("blogPage.blog.publishedOn")} {blog.publish_date}
                         </p>
-
-                        <h1 className='py-2 font-sans'>Heading 1</h1>
-                        <h2 className='py-2 font-sans'>Heading 2</h2>
-                        <h3 className='py-2 font-sans'>Heading 3</h3>
-                        <h4 className='py-2 font-sans'>Heading 4</h4>
-                        <h5 className='py-2 font-sans'>Heading 5</h5>
-                        <h6 className='py-2 font-sans'>Heading 6</h6>
-
-                        <p className='py-6'>
-                            Sed dignissim lectus ut tincidunt vulputate. Fusce
-                            tincidunt lacus purus, in mattis tortor sollicitudin
-                            pretium. Phasellus at diam posuere, scelerisque nisl
-                            sit amet, tincidunt urna. Cras nisi diam, pulvinar
-                            ut molestie eget, eleifend ac magna. Sed at lorem
-                            condimentum, dignissim lorem eu, blandit massa.
-                            Phasellus eleifend turpis vel erat bibendum
-                            scelerisque. Maecenas id risus dictum, rhoncus odio
-                            vitae, maximus purus. Etiam efficitur dolor in dolor
-                            molestie ornare. Aenean pulvinar diam nec neque
-                            tincidunt, vitae molestie quam fermentum. Donec ac
-                            pretium diam. Suspendisse sed odio risus. Nunc nec
-                            luctus nisi. className aptent taciti sociosqu ad
-                            litora torquent per conubia nostra, per inceptos
-                            himenaeos. Duis nec nulla eget sem dictum elementum.
-                        </p>
-
-                        <ol>
-                            <li className='py-3'>
-                                Maecenas accumsan lacus sit amet elementum
-                                porta. Aliquam eu libero lectus. Fusce vehicula
-                                dictum mi. In non dolor at sem ullamcorper
-                                venenatis ut sed dui. Ut ut est quam.
-                                Suspendisse quam quam, commodo sit amet placerat
-                                in, interdum a ipsum. Morbi sit amet tellus
-                                scelerisque tortor semper posuere.
-                            </li>
-                            <li className='py-3'>
-                                Morbi varius posuere blandit. Praesent gravida
-                                bibendum neque eget commodo. Duis auctor ornare
-                                mauris, eu accumsan odio viverra in. Proin
-                                sagittis maximus pharetra. Nullam lorem mauris,
-                                faucibus ut odio tempus, ultrices aliquet ex.
-                                Nam id quam eget ipsum luctus hendrerit. Ut eros
-                                magna, eleifend ac ornare vulputate, pretium nec
-                                felis.
-                            </li>
-                            <li className='py-3'>
-                                Vestibulum ante ipsum primis in faucibus orci
-                                luctus et ultrices posuere cubilia Curae; Nunc
-                                vitae pretium elit. Cras leo mauris, tristique
-                                in risus ac, tristique rutrum velit. Mauris
-                                accumsan tempor felis vitae gravida. Cras
-                                egestas convallis malesuada. Etiam ac ante id
-                                tortor vulputate pretium. Maecenas vel sapien
-                                suscipit, elementum odio et, consequat tellus.
-                            </li>
-                        </ol>
-                        {/* Post Content */}
                     </div>
-
-                    {/* Tags */}
-                    <div className='text-base md:text-sm text-gray-500 px-4 py-6'>
-                        Tags:{" "}
-                        <a
-                            href='#'
-                            className='text-base md:text-sm text-green-500 no-underline hover:underline'
-                        >
-                            Link
-                        </a>{" "}
-                        .{" "}
-                        <a
-                            href='#'
-                            className='text-base md:text-sm text-green-500 no-underline hover:underline'
-                        >
-                            Link
-                        </a>
-                    </div>
-
-                    {/* Author */}
-                    <div className='flex w-full items-center font-sans px-4 py-12'>
+                    <div className='py-3'>
                         <img
-                            className='w-10 h-10 rounded-full mr-4'
-                            src='http://i.pravatar.cc/300'
-                            alt='Avatar of Author'
+                            src='/images/fatoom_in_yemen.png'
+                            alt='Image Description'
+                            className=' block'
                         />
-                        <div className='flex-1 px-2'>
-                            <p className='text-base font-bold text-base md:text-xl leading-none mb-2'>
-                                Jo Bloggerson
+                    </div>
+                    {/* Post Content */}
+                    {/* Lead Para */}
+
+                    <div>
+                        {paragraphs.map((paragraph, index) => (
+                            <p className='py-5 dark:text-white' key={index}>
+                                {paragraph}
                             </p>
-                            <p className='text-gray-600 text-xs md:text-base'>
-                                Minimal Blog Tailwind CSS template by
-                            </p>
-                        </div>
-                        {/* Container */}
+                        ))}
                     </div>
                 </div>
+
+                {/* Tags */}
+                <div
+                    className={`flex dark:text-white text-base md:text-sm text-gray-500 ${
+                        router.locale === "en" ? "pl-6" : "pr-6"
+                    } py-6`}
+                >
+                    {t("blogPage.blog.tags")}:
+                    {blog.tags.map((tag, index) => (
+                        <div className='flex' key={index}>
+                            <p
+                                href='#'
+                                className={`${
+                                    router.locale === "en" ? "pl-1" : "pr-1"
+                                } text-base md:text-sm text-green-500 no-underline hover:underline`}
+                            >
+                                {tag}
+                            </p>
+                            {index < blog.tags.length - 1 ? ", " : ". "}
+                        </div>
+                    ))}
+                </div>
+
+                {/* Author */}
+                <div className='flex w-full dark:text-white items-center font-sans px-4 py-12'>
+                    <img
+                        className='w-10 h-10 rounded-full mr-4'
+                        src='http://i.pravatar.cc/300'
+                        alt='Avatar of Author'
+                    />
+                    <div className='flex-1 px-2 pt-1'>
+                        <p className='text-base font-bold text-base md:text-xl leading-none mb-2'>
+                            {blog.author}
+                        </p>
+                    </div>
+                    {/* Container */}
+                </div>
+            </div>{" "}
+            <hr className='mx-20' />
+            <div>
+                <BlogCardList
+                    language={router.locale}
+                    blogs={similarBlogs}
+                    numToShow={3}
+                />
             </div>
         </Layout>
     );
+}
+export async function getServerSideProps({ locale, params }) {
+    const dataBlogs = [];
+    const docRef = doc(db, "blogs", params.blog);
+    const docSnap = await getDoc(docRef);
+    const blog = docSnap.data();
+    const q = query(
+        collection(db, "blogs"),
+        where("type", "==", "article"),
+        where("tags", "array-contains-any", blog.tags)
+    );
+    const queryBlog = await getDocs(q);
+    queryBlog.forEach((doc) => {
+        dataBlogs.push({ id: doc.id, data: doc.data() });
+    });
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["common"])),
+            blog: blog,
+            similarBlogs: dataBlogs,
+        },
+    };
 }
