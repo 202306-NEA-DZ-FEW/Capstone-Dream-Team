3d4e1c7b6d7e160f9c9dc2f7673ac956e907f14d
MerzoukFatima <merzoukfatimazahraa@gmail.com>
2023-11-11 01:42:34 +0100

    feat(chart component): fix x axe and styling
    #71

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,21 +1,11 @@
 import React from "react";
 
-import ChartGraph from "./analyticsChart";
+import ChartGraph from "./chartGraph";
 import StatCardList from "./statCardList";
 
 export default function Overview() {
-    const date = new Date(2023, 10, 11); // Month is zero-based, so 10 represents November
-    const formattedDate = new Intl.DateTimeFormat("en-US", {
-        weekday: "long",
-    }).format(date);
-
-    // Now 'formattedDate' contains the full name of the day (e.g., "Monday")
-
-    // Push 'formattedDate' to Firebase along with other data
-    // firebase.database().ref('yourPath').push({ date: formattedDate, otherData: yourOtherData });
-
     return (
-        <div className=''>
+        <div>
             <StatCardList />
             <ChartGraph />
         </div>
--- src/components/Overview/chartGraph.jsx
+++ src/components/Overview/chartGraph.jsx
@@ -1,15 +1,3 @@
-import {
-    BarElement,
-    CategoryScale,
-    Chart,
-    Legend,
-    LinearScale,
-    LineElement,
-    PointElement,
-    TimeScale,
-    Title,
-    Tooltip,
-} from "chart.js";
 import { onAuthStateChanged } from "firebase/auth";
 import {
     collection,
@@ -18,72 +6,18 @@ import {
     query,
     where,
 } from "firebase/firestore";
+import dynamic from "next/dynamic";
 import React, { useEffect, useState } from "react";
-import { Line } from "react-chartjs-2";
 
 import { auth, db } from "@/util/firebase";
 
+const ReactApexChart = dynamic(() => import("react-apexcharts"), {
+    ssr: false,
+});
+
 function ChartGraph() {
     const [authUser, setAuthUser] = useState(null);
-    const [chartData, setChartData] = useState(null); // Changed variable name
-    // const options = {
-    //     responsive: true,
-    //     plugins: {
-    //         legend: {
-    //             position: "top",
-    //         },
-    //         title: {
-    //             display: true,
-    //             text: "Chart.js Line Chart",
-    //         },
-    //     },
-    //     scales: {
-    //         x: {
-    //             beginAtZero: true,
-    //         },
-    //         y: {
-    //             beginAtZero: true,
-    //             ticks: {
-    //                 // Your custom Y-axis values here
-    //                 stepSize: 5, // You can set the step size
-    //                 min: 0, // Minimum value
-    //                 max: 20, // Maximum value
-    //             },
-    //         },
-    //     },
-    // };
-    const options = {
-        plugins: {
-            legend: {
-                labels: {
-                    color: "red",
-                },
-            },
-        },
-        scales: {
-            x: {
-                ticks: {
-                    color: "green",
-                },
-            },
-            y: {
-                ticks: {
-                    color: "black",
-                },
-            },
-        },
-    };
-    Chart.register(
-        CategoryScale,
-        LinearScale,
-        PointElement,
-        LineElement,
-        Title,
-        Tooltip,
-        Legend,
-        TimeScale,
-        BarElement
-    );
+    const [chartData, setChartData] = useState(null);
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             user ? setAuthUser(user) : setAuthUser(null);
@@ -98,35 +32,98 @@ function ChartGraph() {
                 );
 
                 const querySnapshot = await getDocs(q);
-                const chart = {};
+                let chart = {};
 
                 querySnapshot.forEach((doc) => {
                     const meal = doc.data();
+                    // const dateParts = meal.date.split("/");
+                    // const formattedDate = new Date(
+                    //     dateParts[2],
+                    //     dateParts[1] - 1,
+                    //     dateParts[0]
+                    // );
+                    // const day = new Intl.DateTimeFormat("en-Us", {
+                    //     weekday: "long",
+                    // }).format(formattedDate);
                     const date = meal.date;
-
                     if (chart[date]) {
                         chart[date]++;
                     } else {
                         chart[date] = 1;
                     }
                 });
+                const chartArray = Object.keys(chart)
+                    .map((date) => {
+                        const dateParts = date.split("/");
+                        const formattedDate = new Date(
+                            dateParts[2],
+                            dateParts[1] - 1,
+                            dateParts[0]
+                        );
 
-                const chartArray = Object.keys(chart).map((date) => ({
-                    date: date,
-                    count: chart[date],
-                }));
-                const chartData = {
-                    labels: chartArray.map((data) => data.date),
-                    datasets: [
-                        {
-                            label: "Donation per day",
-                            data: chartArray.map((data) => data.count),
-                            borderColor: "rgb(255, 99, 132)",
-                            backgroundColor: "rgba(255, 99, 132, 0.5)",
+                        return {
+                            day: new Intl.DateTimeFormat("en-US", {
+                                weekday: "long",
+                            }).format(formattedDate),
+                            date: date,
+                            count: chart[date],
+                            formattedDate: formattedDate,
+                        };
+                    })
+                    .sort((a, b) => a.formattedDate - b.formattedDate);
+
+                // Add a null check before mapping
+                if (chartArray && chartArray.length > 0) {
+                    const chartData = {
+                        series: [
+                            {
+                                name: "Donation per day",
+                                data: chartArray.map((data) => data.count),
+                            },
+                        ],
+                        options: {
+                            stroke: {
+                                curve: "smooth",
+                            },
+
+                            xaxis: {
+                                type: "category",
+                                categories: chartArray.map((data) => data.day),
+                            },
+                            yaxis: {
+                                type: "numeric",
+                                min: 0,
+                                labels: {
+                                    style: {
+                                        colors: "#575757",
+                                        fontSize: "14px",
+                                    },
+                                },
+                                forceNiceScale: true,
+                            },
+                            tooltip: {
+                                custom: function ({
+                                    series,
+                                    seriesIndex,
+                                    dataPointIndex,
+                                    w,
+                                }) {
+                                    const data = chartArray[dataPointIndex];
+                                    return (
+                                        `<div style="background-color: #ffffff; padding: 10px; border: 1px solid #cccccc; border-radius: 5px;">` +
+                                        `<strong>Date:</strong> ${data.formattedDate.toLocaleDateString()}<br>` +
+                                        `<strong>Day:</strong> ${data.day}<br>` +
+                                        `<strong>Count:</strong> ${data.count}</div>`
+                                    );
+                                },
+                            },
                         },
-                    ],
-                };
-                setChartData(chartData); // Corrected variable name
+                    };
+                    setChartData(chartData);
+                } else {
+                    // Handle the case when chartArray is null or empty
+                    setChartData(null);
+                }
             }
         };
 
@@ -140,11 +137,22 @@ function ChartGraph() {
     }, [authUser]);
 
     return (
-        <div className='h-2/3 max-w-full bg-white'>
-            hey
-            {chartData && (
-                <Line className='border' data={chartData} options={options} />
-            )}
+        <div className='col-span-12 rounded-md border border-stroke bg-white px-5 pt-7 pb-7 shadow sm:px-7.5 xl:col-span-8'>
+            <div className='flex w-full flex-col flex-wrap items-start justify-between gap-3 sm:flex-nowrap'>
+                <p className='font-bold text-primary'>Donation per day </p>
+                <div className='w-full'>
+                    <div className='-ml-5'>
+                        {chartData && (
+                            <ReactApexChart
+                                options={chartData.options}
+                                series={chartData.series}
+                                type='area'
+                                height={350}
+                            />
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
