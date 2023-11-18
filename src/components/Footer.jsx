f18d14a503922c6338d0647375f9369c8fb0cfa6
zaidi mounib <mounibzaidi@gmail.com>
2023-11-18 22:04:01 +0100

    feat(component): update styling
    #9

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,65 +5,51 @@ export default function Footer() {
     const { t } = useTranslation("common");
 
     return (
-        <footer className='bg-white h-100 max-w-screen-xl mx-auto text-[#192655] p-6'>
-            <div className='flex items-center flex-row '>
-                <img
-                    src='images/home/Navbar/logo.png'
-                    alt='Logo'
-                    width='100'
-                    height='100'
-                />
-
-                <div className=' ml-auto'>
-                    <h2 className='font-bold mb-2'>
-                        Subscribe to our newsletter
-                    </h2>
-                    <form action='#'>
-                        <input
-                            type='email'
-                            placeholder='Email'
-                            className='mb-2 h-10'
-                        />
-                        <button
-                            className='border p-2 bg-amber-400'
-                            type='submit'
-                        >
-                            Submit
-                        </button>
-                    </form>
-                </div>
-            </div>
-
-            <div className='flex flex-col md:flex-row justify-between '>
-                <div className='text-center md:text-left'>
-                    <p>© 2021 Buy me a meal. All rights reserved.</p>
-                    <p>3348 Nw 23rd Ave, Suite A, Gainesville, FL 32605</p>
-                    <p>+1 352-505-9106</p>
-                    <p>support@buymeameal.com</p>
-                </div>
-
-                <div className='font-bold mb-2 md:flex md:flex-col md:items-center flex-grid justify-center'>
-                    <Link href='/home' passHref>
-                        <h1 className='cursor-pointer'>{t("footer.home")}</h1>
-                    </Link>
-                    <Link href='/meals' passHref>
-                        <h1 className='cursor-pointer'>{t("footer.meals")}</h1>
-                    </Link>
-                    <Link href='/blog' passHref>
-                        <h1 className='cursor-pointer'>{t("footer.blog")}</h1>
-                    </Link>
-                </div>
-                <div>
-                    <h2 className='font-bold mr-20 mb-2'>
-                        {t("footer.followUs")}
-                    </h2>
-                    <div className='flex flex-col'>
-                        <p href='#'>LinkedIn</p>
-                        <p href='#'>Instagram</p>
-                        <p href='#'>Facebook</p>
+        <section className='bg-white'>
+            <div className='max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8'>
+                <nav className='flex flex-wrap justify-center -mx-5 -my-2'>
+                    <div className='px-5 py-2'>
+                        <Link href='/about' passHref>
+                            <p className='text-base leading-6 text-gray-500 hover:text-gray-900 cursor-pointer'>
+                                {t("footer.home")}
+                            </p>
+                        </Link>
                     </div>
-                </div>
+                    <div className='px-5 py-2'>
+                        <Link href='/blogs' passHref>
+                            <p className='text-base leading-6 text-gray-500 hover:text-gray-900 cursor-pointer'>
+                                {t("footer.blog")}
+                            </p>
+                        </Link>
+                    </div>
+                    <div className='px-5 py-2'>
+                        <Link href='/team' passHref>
+                            <p className='text-base leading-6 text-gray-500 hover:text-gray-900 cursor-pointer'>
+                                {t("footer.meals")}
+                            </p>
+                        </Link>
+                    </div>
+                    <div className='px-5 py-2'>
+                        <Link href='/aboutus' passHref>
+                            <p className='text-base leading-6 text-gray-500 hover:text-gray-900 cursor-pointer'>
+                                {t("footer.aboutus")}
+                            </p>
+                        </Link>
+                    </div>
+                </nav>
+
+                <p className='mt-8 text-base leading-6 text-center text-gray-400'>
+                    <div className='flex items-center justify-center'>
+                        <img
+                            src='images/home/Navbar/logo.png'
+                            alt='Logo'
+                            width='50'
+                            height='50'
+                        />
+                        <span> {t("footer.copyright")}</span>
+                    </div>
+                </p>
             </div>
-        </footer>
+        </section>
     );
 }
