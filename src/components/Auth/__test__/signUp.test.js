0eaddb1aaaef8edfb66e458ffb8dc6df933113cd
Mouloud Mecheter <mouloudmecheter@gmail.com>
2023-11-20 20:05:35 +0100

    test(signup test): do the signUp component test

---
+++ src/components/Auth/__test__/signUp.test.js
@@ -0,0 +1,26 @@
+import renderer from "react-test-renderer";
+import SignUp from "../signUp";
+
+import { getAuth } from "@/util/firebase";
+
+// Mock the Firebase services
+jest.mock("@/util/firebase", () => ({
+    getAuth: jest.fn(),
+}));
+
+// Mock the useRouter hook
+jest.mock("next/router", () => ({
+    useRouter: jest.fn().mockReturnValue({
+        push: jest.fn(),
+    }),
+}));
+
+jest.mock("next-i18next", () => ({
+    useTranslation: jest.fn().mockReturnValue({ t: jest.fn() }),
+}));
+
+it("renders correctly", () => {
+    getAuth.mockReturnValue();
+    const tree = renderer.create(<SignUp />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
