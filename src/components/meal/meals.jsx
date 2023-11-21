de044e04523d386297a7b121c0a882db450a4e76
walidbelharazem <walidbelharazem@gmail.com>
2023-11-21 14:43:18 +0100

    feat: deleted the pagination in order to review it
    deleted the pagination in order to review it ; changed the teamcard ; added the faveicon
    
    #4

--- src/components/meal/meals.jsx
+++ src/components/meal/meals.jsx
@@ -3,7 +3,6 @@ import Mealbar from "./mealbar";
 import React, { useState, useEffect } from "react";
 import { collection, query, getDocs, where } from "firebase/firestore"; // Import Firestore functions
 import { db } from "../../util/firebase"; // Replace with your Firebase config import
-import Bar from "./Bar";
 import ReactPaginate from "react-paginate";
 import { useTranslation } from "next-i18next";
 
@@ -63,63 +62,62 @@ export default function Meals() {
     }, []);
 
     //const [users, setUsers] = useState(JsonData.slice(0, 50));
-    const [pageNumber, setPageNumber] = useState(0);
+    // const [pageNumber, setPageNumber] = useState(0);
 
-    const usersPerPage = 2;
-    const pagesVisited = pageNumber * usersPerPage;
+    // const usersPerPage = 2;
+    // const pagesVisited = pageNumber * usersPerPage;
 
-    const displayUsers = rest
-        .filter((restaurant) => {
-            return search.toLowerCase() === ""
-                ? restaurant
-                : restaurant.Name.toLowerCase().includes(search);
-        })
-        .slice(pagesVisited, pagesVisited + usersPerPage)
-        .map((restaurant, index) => {
-            return (
-                <div className=' mb-5 mt-10 mx-auto ' key={index}>
-                    <div className=' pl-3 pr-3 '>
-                        <Mealbar
-                            name={restaurant.Name}
-                            current_restaurant_Id={
-                                restaurant.current_restaurant_Id
-                            }
-                            image={restaurant.image}
-                        />
-                    </div>
-                    <div className='pb-10 pt-5 xl:mx-1 flex justify-center items-center '>
-                        <div className='grid center grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg2:grid-cols-4 gap-20 md:gap-8 lg:gap-8 lg2:gap-2'>
-                            {restaurant.mealDetails
-                                .slice(0, 4)
-                                .map((mealDetail, mealIndex) => (
-                                    <div
-                                        className='flex md:mb-10'
-                                        key={mealIndex}
-                                    >
-                                        <Mealcard
-                                            price={mealDetail.price}
-                                            maxMeals={mealDetail.maxMeals}
-                                            name={mealDetail.name}
-                                            imageUrl={mealDetail.imageUrl}
-                                            mealDetail={mealDetail}
-                                        />
-                                    </div>
-                                ))}
-                        </div>
-                    </div>
-                </div>
-            );
-        });
+    // const displayUsers = rest
+    //     .filter((restaurant) => {
+    //         return search.toLowerCase() === ""
+    //             ? restaurant
+    //             : restaurant.Name.toLowerCase().includes(search);
+    //     })
+    //     .slice(pagesVisited, pagesVisited + usersPerPage)
+    //     .map((restaurant, index) => {
+    //         return (
+    //             <div className=' mb-5 mt-10 mx-auto ' key={index}>
+    //                 <div className=' pl-3 pr-3 '>
+    //                     <Mealbar
+    //                         name={restaurant.Name}
+    //                         current_restaurant_Id={
+    //                             restaurant.current_restaurant_Id
+    //                         }
+    //                         image={restaurant.image}
+    //                     />
+    //                 </div>
+    //                 <div className='pb-10 pt-5 xl:mx-1 flex justify-center items-center '>
+    //                     <div className='grid center grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg2:grid-cols-4 gap-20 md:gap-8 lg:gap-8 lg2:gap-2'>
+    //                         {restaurant.mealDetails
+    //                             .slice(0, 4)
+    //                             .map((mealDetail, mealIndex) => (
+    //                                 <div
+    //                                     className='flex md:mb-10'
+    //                                     key={mealIndex}
+    //                                 >
+    //                                     <Mealcard
+    //                                         price={mealDetail.price}
+    //                                         maxMeals={mealDetail.maxMeals}
+    //                                         name={mealDetail.name}
+    //                                         imageUrl={mealDetail.imageUrl}
+    //                                         mealDetail={mealDetail}
+    //                                     />
+    //                                 </div>
+    //                             ))}
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         );
+    //     });
 
-    const pageCount = Math.ceil(rest.length / usersPerPage);
+    // const pageCount = Math.ceil(rest.length / usersPerPage);
 
-    const changePage = ({ selected }) => {
-        setPageNumber(selected);
-    };
+    // const changePage = ({ selected }) => {
+    //     setPageNumber(selected);
+    // };
 
     return (
         <div>
-            {/* <Bar /> */}
             {/**search bar*/}
             <div className='py-4 mx-4 md:mx-6'>
                 <div className='mt-4 pb-4 flex space-x-3 border-b border-gray-200 dark:border-gray-700'>
@@ -159,7 +157,7 @@ export default function Meals() {
                 </div>
             </div>
 
-            {/* {rest
+            {rest
                 .filter((restaurant) => {
                     return search.toLowerCase() === ""
                         ? restaurant
@@ -197,9 +195,9 @@ export default function Meals() {
                             </div>
                         </div>
                     </div>
-                ))}*/}
+                ))}
 
-            {displayUsers}
+            {/* {displayUsers}
 
             <div className='mb-5'>
                 <ReactPaginate
@@ -222,7 +220,7 @@ export default function Meals() {
                         "px-4 py-2 border border-gray-300 rounded-full hover:bg-blue-400"
                     }
                 />
-            </div>
+            </div> */}
         </div>
     );
 }
