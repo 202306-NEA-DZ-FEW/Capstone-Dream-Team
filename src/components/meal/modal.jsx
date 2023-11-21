3266ccb5967410b4ec7c972b38f0ceb48c6f0ddd
walidbelharazem <walidbelharazem@gmail.com>
2023-11-21 18:11:17 +0100

    feat: adjust the styling of the modal meals

--- src/components/meal/modal.jsx
+++ src/components/meal/modal.jsx
@@ -71,33 +71,37 @@ const Modal = ({ currentRestaurantId }) => {
                         {/* Modal content */}
                         <div className='relative bg-white border-4 border-blue-500 rounded-lg shadow dark:bg-gray-700'>
                             {/* TITLE & HIDE X */}
-                            <div className='flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600'>
-                                <h3 className='text-xl font-semibold text-gray-900 dark:text-white'>
-                                    {" "}
-                                    {t("mealsPage.available_meals")}{" "}
-                                </h3>
-                                <button
-                                    type='button'
-                                    className='text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover-bg-gray-600 dark:hover-text-white'
-                                    data-modal-hide='default-modal'
-                                    onClick={hideModal}
-                                >
-                                    <svg
-                                        className='w-3 h-3'
-                                        aria-hidden='true'
-                                        xmlns='http://www.w3.org/2000/svg'
-                                        fill='none'
-                                        viewBox='0 0 14 14'
+                            <div className='flex items-center  justify-between p-4 border-b rounded-t dark:border-gray-600'>
+                                <div>
+                                    <h3 className='text-xl font-semibold text-gray-900 dark:text-white'>
+                                        {" "}
+                                        {t("mealsPage.available_meals")}{" "}
+                                    </h3>
+                                </div>
+                                <div>
+                                    <button
+                                        type='button'
+                                        className='text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover-bg-gray-600 dark:hover-text-white'
+                                        data-modal-hide='default-modal'
+                                        onClick={hideModal}
                                     >
-                                        <path
-                                            stroke='currentColor'
-                                            stroke-linecap='round'
-                                            stroke-linejoin='round'
-                                            stroke-width='2'
-                                            d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'
-                                        />
-                                    </svg>
-                                </button>
+                                        <svg
+                                            className='w-3 h-3'
+                                            aria-hidden='true'
+                                            xmlns='http://www.w3.org/2000/svg'
+                                            fill='none'
+                                            viewBox='0 0 14 14'
+                                        >
+                                            <path
+                                                stroke='currentColor'
+                                                stroke-linecap='round'
+                                                stroke-linejoin='round'
+                                                stroke-width='2'
+                                                d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'
+                                            />
+                                        </svg>
+                                    </button>
+                                </div>
                             </div>
 
                             {/* Modal body */}
