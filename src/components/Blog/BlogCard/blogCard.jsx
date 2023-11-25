9d755a30a5d4cc25edbd1b8552784b9ba524517d
aboura <bouchrabichou638@gmail.com>
2023-11-25 23:21:53 +0100

    fix(homepage): fix styling

--- src/components/Blog/BlogCard/blogCard.jsx
+++ src/components/Blog/BlogCard/blogCard.jsx
@@ -21,7 +21,7 @@ export default function BlogCard({ blog }) {
                     <div className='group-hover:bg-transparent transition rounded-xl duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-700 opacity-25'></div>
                 </div>
                 <div className='py-2'>
-                    <p className='w-80 text-stone-900 text-xl font-medium capitalize line-clamp-2 group-hover:text-blue-700'>
+                    <p className='w-80 bg-white rounded-xl p-2 text-stone-900 text-xl font-medium capitalize line-clamp-2 group-hover:text-blue-700'>
                         {blog.data.title}
                     </p>
                 </div>
@@ -32,7 +32,7 @@ export default function BlogCard({ blog }) {
                     </p>
 
                     <div className='flex'>
-                        <span className='text-gray-600 dark:text-white inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-md'>
+                        <span className='text-gray-400 dark:text-white inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-md'>
                             {blog.data.publish_date}
                         </span>
                     </div>
--- src/components/Home/Donation.jsx
+++ src/components/Home/Donation.jsx
@@ -4,7 +4,7 @@ export default function Donation() {
     return (
         <>
             <div
-                className='px-4 py-16 mx-auto my-20 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 bg-gray-100 bg-cover'
+                className='px-4 py-16 mx-auto my-20 sm:max-w-xl md:max-w-full lg:max-w-screen-2xl md:px-24 lg:px-8 lg:py-20 bg-gray-100 bg-cover'
                 style={{
                     backgroundImage:
                         'url("/images/home/donation/bannerhero.png")',
