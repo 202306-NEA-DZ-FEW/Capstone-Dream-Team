9c9c85350833335b046cbb64804743e375537847
MerzoukFatima <merzoukfatimazahraa@gmail.com>
2023-11-27 01:24:16 +0100

    style(home page): style home page component #115

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,29 +9,30 @@ export default function Footer() {
             <div className='max-w-screen-xl px-4 py-8 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8'>
                 <nav className='flex flex-wrap justify-center space-x-8 -mx-5 -my-2'>
                     <div className='px-5 py-2'>
-                        <Link href='/about' passHref>
-                            <p className='text-base leading-6 text-white hover:text-gray-900 cursor-pointer'>
+                        <Link href='/' passHref>
+                            <p className='text-base leading-6 text-white hover:text-orange-400 cursor-pointer'>
                                 {t("footer.home")}
                             </p>
                         </Link>
                     </div>
+
                     <div className='px-5 py-2'>
-                        <Link href='/blogs' passHref>
-                            <p className='text-base leading-6 text-white hover:text-gray-900 cursor-pointer'>
-                                {t("footer.blog")}
-                            </p>
-                        </Link>
-                    </div>
-                    <div className='px-5 py-2'>
-                        <Link href='/team' passHref>
-                            <p className='text-base leading-6 text-white hover:text-gray-900 cursor-pointer'>
+                        <Link href='/meals' passHref>
+                            <p className='text-base leading-6 text-white hover:text-orange-400 cursor-pointer'>
                                 {t("footer.meals")}
                             </p>
                         </Link>
                     </div>
                     <div className='px-5 py-2'>
+                        <Link href='/blogs' passHref>
+                            <p className='text-base leading-6 text-white hover:text-orange-400 cursor-pointer'>
+                                {t("footer.blog")}
+                            </p>
+                        </Link>
+                    </div>
+                    <div className='px-5 py-2'>
                         <Link href='/aboutus' passHref>
-                            <p className='text-base leading-6 text-white hover:text-gray-900 cursor-pointer'>
+                            <p className='text-base leading-6 text-white hover:text-orange-400 cursor-pointer'>
                                 {t("footer.aboutUs")}
                             </p>
                         </Link>
